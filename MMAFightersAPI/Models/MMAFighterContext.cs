ea1195cb6e75b6d97dc3aff808c4f66dbe126e36
Malik El Guermai <malikelguermai@outlook.com>
2024-04-21 22:17:51 -0400

    Adding the MMAFighterContext database context

--- MMAFightersAPI/Models/MMAFighterContext.cs
+++ MMAFightersAPI/Models/MMAFighterContext.cs
@@ -1,6 +1,15 @@
-﻿namespace MMAFightersAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+//using System.Diagnostics.CodeAnalysis;
+
+namespace MMAFightersAPI.Models
 {
-    public class MMAFighterContext
+    public class MMAFighterContext : DbContext
     {
+        public MMAFighterContext(DbContextOptions<MMAFighterContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MMAFighter> MMAFighters { get; set; } = null!;
     }
 }
